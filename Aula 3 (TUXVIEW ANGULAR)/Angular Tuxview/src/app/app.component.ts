7914753b94e378360ac44b3e7cddc196643d3a65
WL <96277058+walzuguir@users.noreply.github.com>
2022-11-21 19:08:29 -0300

    Aula 3 (Trabalho 3)
    Tuxview, projeto angular Aula 3.

---
+++ Aula 3 (TUXVIEW ANGULAR)/Angular Tuxview/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Trabalho de Angular 3';
+  aluno = "Pedro Cabral";
+
+  imagens = [
+    "https://cdn.pixabay.com/photo/2022/10/15/21/23/cat-7523894_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/07/15/09/00/man-845847_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/04/23/12/32/hell-735995_960_720.jpg",
+    "https://media.istockphoto.com/photos/global-communication-network-concept-social-media-worldwide-business-picture-id1204762097?s=612x612"
+  ]
+
+
+
+}
